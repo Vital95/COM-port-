ae71cda6738076db86accca270937dbb0de2ef8d
Vitalii Shevchuk <1vitaliy.shevchuk1995@gmail.com>
2015-12-06 01:39:54 +0200

    bug fixed

--- ComPort/ComPort/Form1.Designer.cs
+++ ComPort/ComPort/Form1.Designer.cs
@@ -130,6 +130,7 @@
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(360, 9);
             this.label1.Name = "label1";
