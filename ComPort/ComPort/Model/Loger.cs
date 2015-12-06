467d2d83e6e73788aeb09c4be265907fe90e892c
RomaReznichenko <roma.reznschenko.95@mail.ru>
2015-12-06 20:59:01 +0200

    loger

---
+++ ComPort/ComPort/Model/Loger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ComPort
+{
+    static class Loger
+    {
+        static void SetLog(String data)
+        {
+            StreamWriter file = new StreamWriter("Log.txt");
+            file.WriteLine(data);
+
+            file.Close();
+        }
+    }
+}
