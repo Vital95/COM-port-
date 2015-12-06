d239e61acfdfaf338f9e797e8dd15d416320e705
Vitalii Shevchuk <1vitaliy.shevchuk1995@gmail.com>
2015-12-06 02:35:25 +0200

    тф

---
+++ ComPort/ComPort/Helper/Parser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPort
+{
+    class Parser
+    {
+        public Data GetParesedData(String data)
+        {
+
+        }
+
+        private bool ValidateData(String data)
+        {
+
+        }
+    }
+}
---
+++ ComPort/ComPort/Model/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPort
+{
+    class Data
+    {
+        int value { get; set; }
+        int status { get; set; }
+        int id { get; set; }
+    }
+}
