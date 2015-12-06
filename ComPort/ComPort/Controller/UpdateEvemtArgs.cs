da7079ae4c7a07378471913b04b9a28fbf116f11
RomaReznichenko <roma.reznschenko.95@mail.ru>
2015-12-06 03:46:12 +0200

    fff

---
+++ ComPort/ComPort/Controller/UpdateEvemtArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComPort
+{
+    class UpdateEventArgs : EventArgs
+    {
+        private Data info;
+
+        UpdateEventArgs(Data info)
+        {
+            this.info = info;
+        }
+        public Data Info
+        {
+            get
+            {
+                return info;
+            }
+            set
+            {
+                this.info = Info;
+            }
+        }
+    }
+}
